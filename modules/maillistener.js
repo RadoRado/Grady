36fcfba3d3da3649f539c8a651f6bedd0bf83b71
Radoslav Georgiev <g2h.horde@gmail.com>
2014-08-14 17:18:55 +0300

    Initial structure commit
    - Basic mail-watcher functionality working

---
+++ modules/maillistener.js
@@ -0,0 +1,20 @@
+var
+  MailListener = require("mail-listener2"),
+  config = require("../config"),
+  mailListener = new MailListener({
+    username: config.username,
+    password: config.password,
+    host: config.host,
+    port: config.port, // imap port
+    tls: true,
+    tlsOptions: {
+      rejectUnauthorized: false
+    },
+    mailbox: config.mailbox, // mailbox to monitor
+    searchFilter: config.searchFilter, // the search filter being used after an IDLE notification has been retrieved
+    markSeen: config.markSeen, // all fetched email willbe marked as seen and not fetched next time
+    fetchUnreadOnStart: config.fetchUnreadOnStart, // use it only if you want to get all unread email on lib start. Default is `false`,
+    attachments: false
+});
+
+module.exports = mailListener;
---
+++ server.js
@@ -0,0 +1,94 @@
+/* global require, console */
+'use strict';
+
+var
+  config = require('./config'),
+  mailListener = require('./modules/maillistener'),
+  mongoose = require('mongoose'),
+  mailSchema = mongoose.Schema({
+    mailId: String,
+    subject: String,
+    body: String,
+    from: String,
+    fromName: String,
+    receivedDate: Date,
+    attachments: [{
+      filename: String,
+      contents: String
+    }]
+  }),
+  Mail = mongoose.model('Mail', mailSchema),
+  StringDecoder = require('string_decoder').StringDecoder;
+
+mongoose.connect(config.mongoConnectionString);
+
+mailListener.on('server:connected', function() {
+  console.log('imapConnected');
+});
+
+mailListener.on('server:disconnected', function() {
+  console.log('imapDisconnected');
+});
+
+mailListener.on('error', function(err){
+  console.log(err);
+});
+
+mailListener.on('mail', actOnNewEmail);
+
+
+function actOnNewEmail(mail) {
+  if(!mail.attachments) {
+    addTaskToQueue({
+      type: 'email',
+      data: {
+        to: mail.from[0].address,
+        message: 'FYI - Nothing was attached in the email you sent.'
+      }
+    });
+    return;
+  }
+
+  storeMail(mail);
+}
+
+function addTaskToQueue(task) {
+  console.log('Should add task to queue');
+  console.log(task);
+}
+
+function storeMail(mail) {
+  var
+    attachments = [],
+    received = {};
+
+  mail.attachments.forEach(function(attachment) {
+    var decoder = new StringDecoder('utf8');
+
+    attachments.push({
+      filename: attachment.fileName,
+      contents: decoder.write(attachment.content)
+    });
+  });
+
+  received = new Mail({
+    mailId: mail.messageId,
+    subject: mail.subject,
+    body: mail.text,
+    from: mail.from[0].address,
+    fromName: mail.from[0].name,
+    receivedDate: new Date(Date.parse(mail.date)),
+    attachments: attachments
+  });
+
+  received.save(function(err) {
+    if(err) {
+      console.log('Error on saving mail model!');
+      console.log(err);
+    }
+
+    console.log('Mail saved to database.');
+  });
+}
+
+mailListener.start();
