4d0f23af4407f3dfb6592a1d8e9243a17e11301c
Radoslav Georgiev <g2h.horde@gmail.com>
2014-08-15 17:48:48 +0300

    Adding a simple worker to test things out
    - Code is a huge mess

--- mail-watcher/modules/queuer.js
+++ mail-watcher/modules/queuer.js
@@ -1,15 +1,30 @@
 var
     redis = require("redis"),
-    client = redis.createClient();
+    publishClient = redis.createClient(),
+    subscriberClient = redis.createClient(),
+    putTasksClient = redis.createClient();
+
+subscriberClient.on("subscribe", function(channel) {
+    console.log("Subscribed to " + channel);
+    console.log("Will be notified if worker dispatcher comes alive");
+})
+
+subscriberClient.on("message", function(channel, message) {
+    console.log("Worker is alive!");
+    Queuer.notifyWorkers("notify_on_new_task", "task_waiting_queue");
+});
+subscriberClient.subscribe("status");
 
 function Queuer() {}
 
 Queuer.addTask = function(task) {
-    client.LPUSH("task-waiting-queue", JSON.stringify(task), redis.print);
+    putTasksClient.LPUSH("task_waiting_queue", JSON.stringify(task), redis.print);
+    Queuer.notifyWorkers("notify_on_new_task", "task_waiting_queue")
 }
 
 Queuer.notifyWorkers = function(channel, message) {
-
+    publishClient.publish(channel, message);
 }
 
 module.exports = Queuer;
+
