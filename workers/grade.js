d2bb870be20000aeb696d7d8ac5f232462b3c7bf
Radoslav Georgiev <g2h.horde@gmail.com>
2014-08-24 14:18:37 +0300

    Adding functionality to pick the right worker

--- workers/grade.js
+++ workers/grade.js
@@ -1,5 +1,23 @@
+var
+  path = require("path"),
+  gradersByExtension = {
+    ".scm": "grader.scm.js"
+  };
+
 module.exports = function (input, callback) {
-  console.log(input);
+
+  input.attachments.forEach(function(attachment) {
+    var
+      fileExtension = path.extname(attachment.filename),
+      grader = gradersByExtension[fileExtension];
+
+    if(!grader) {
+      console.log("Cannot grade %s", fileExtension);
+      return;
+    }
+
+    console.log("Will grade %s with %s", attachment.filename, grader);
+  });
 
   callback(null, "Grading stuff");
 };
